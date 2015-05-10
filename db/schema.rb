0ec4557e3661f6685c5aa21bd13d355d3ad2562a
ryuk03 <ryukg03@gmail.com>
2015-05-10 11:31:09 -0500

    Add account activations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509160221) do
+ActiveRecord::Schema.define(version: 20150510152915) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
