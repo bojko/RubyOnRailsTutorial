84cf939741742d797bf2facc8e1d2a118c7255e7
ryuk03 <ryukg03@gmail.com>
2015-05-13 09:18:44 -0500

    Fix merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
-  attr_accessor :remember_token, :activation_token
-  before_save :downcase_email
-  before_save :create_activation_digest
-  attr_accessor :remember_token, :activation_token, :reset_token
-  before_save :downcase_email
-  before_create :create_activation_digest
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -56,65 +50,33 @@ class User < ActiveRecord::Base
     # Creates and assigns the activation token and digest.
     def create_activation_digest
       self.activation_token  = User.new_token
-  end
+    end
   
-  # Activates an account.
-  def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
+    # Activates an account.
+    def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+    end
 
-  # Sends activation email.
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
+    # Sends activation email.
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
 
-  # Sets the password reset attributes.
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
-  
-  # Activates an account.
-  def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
+    # Sets the password reset attributes.
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest,  User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+    end
 
-  # Sends activation email.
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
-
-  # Sets the password reset attributes.
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
->>>>>>> account-activation-password-reset
-
-  # Sends password reset email.
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
-  end
-  
-  # Returns true if a password reset has expired.
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
-  
-  private
-    
-    # Converts email to all lower-case
-    def downcase_email
-      self.email = email.downcase
+    # Sends password reset email.
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
     end
     
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150510171027) do
-=======
 ActiveRecord::Schema.define(version: 20150512164215) do
 
   create_table "microposts", force: :cascade do |t|
@@ -25,7 +22,6 @@ ActiveRecord::Schema.define(version: 20150512164215) do
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
->>>>>>> user-microposts
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -38,11 +34,8 @@ ActiveRecord::Schema.define(version: 20150512164215) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
-<<<<<<< HEAD
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
-=======
->>>>>>> user-microposts
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
